20468c46b15502ae26df8681ae165d7b13a0412c
Carlos Lopes <dev.carloslopes@gmail.com>
2022-02-21 20:38:27 -0300

    participantes adicionado a lista

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ const fetchData = async (url) => {
 
 fetchData("https://run.mocky.io/v3/349d9b08-b21b-4134-a500-883efcc6f8f6").then(data => {
   const rooms = data.salas;
-  console.log(rooms)
   const roomTitle = document.querySelector(".nav__title");
   const participants = document.querySelector(".chat-participants__title");
 
@@ -20,10 +19,11 @@ fetchData("https://run.mocky.io/v3/349d9b08-b21b-4134-a500-883efcc6f8f6").then(d
 
 fetchData("https://run.mocky.io/v3/55e3f9c9-df86-46c9-941a-ea43a7595e19").then(data => {
   const messages = data.messages;
-  console.log(messages)
   const chatMessage = document.getElementById("chat-message");
+  const roomList = document.getElementById("room-list");
 
   messages.map(message => {
+    roomList.innerHTML += `<li class="room-list__item">${message.userName}</li>`;
     chatMessage.innerHTML += `
     <li class="chat-message__container">
       <p class="chat-message__user">${message.userName}</p>
