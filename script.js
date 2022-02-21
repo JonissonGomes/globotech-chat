f12bd831ad45d5e285cf8ead77294c31721e4f9e
JonissonGomes <joni_gomes@hotmail.com>
2022-02-21 19:55:11 -0300

    Removendo função getRooms

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
+let response = {}
+
 const fetchRooms = () => {
   return fetch("https://run.mocky.io/v3/349d9b08-b21b-4134-a500-883efcc6f8f6")
     .then(response => response.json())
     .then(rooms => {
-      return rooms;
+      response = rooms
+      return response;
     }).catch(err => {
       console.error('Not Found', err);
     }); ;
 }
 
-const getRooms = async () => {
-  const rooms = await fetchRooms();
-
-  return rooms;
-}
-
-getRooms();
+fetchRooms();
