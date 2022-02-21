cfef9796db5b97bd6286e9a4d5e174bd85d5bdda
Carlos Lopes <dev.carloslopes@gmail.com>
2022-02-21 19:59:53 -0300

    refactor: alteração na função e retorno fetch

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-const fetchRooms = () => {
-  return fetch("https://run.mocky.io/v3/349d9b08-b21b-4134-a500-883efcc6f8f6")
-    .then(response => response.json())
-    .then(rooms => {
-      return rooms;
-    }).catch(err => {
-      console.error('Not Found', err);
-    }); ;
+const fetchRooms = async (url) => {
+  const response = await fetch(url);
+  
+  const json = await response.json();
+
+  return json;
 }
 
-const getRooms = async () => {
-  const rooms = await fetchRooms();
+fetchRooms("https://run.mocky.io/v3/349d9b08-b21b-4134-a500-883efcc6f8f6").then(data => {
+  const rooms = data.salas;
+  const roomList = document.getElementById("room-list");
+  rooms.map(room => {
+    const roomItem = document.createElement("li");
+    roomItem.innerHTML = `
+      <h3>${room.name}</h3>
+    `;
 
-  return rooms;
-}
-
-getRooms();
+    roomList.appendChild(roomItem);
+  });
+})
