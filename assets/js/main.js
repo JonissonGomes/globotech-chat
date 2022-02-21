5329b12ccdff9a5764baa692fe2985b0f9a4e8ed
vargasraquel <raquelvargas@gmail.com>
2022-02-21 20:46:56 -0300

    Adding own message to chatbox

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
 const containerEl = document.querySelector(".chat-messages__container");
 
-
 fetch("https://run.mocky.io/v3/55e3f9c9-df86-46c9-941a-ea43a7595e19")
   .then((response) => response.json())
   .then((data) => {const result = data.messages
@@ -16,3 +15,21 @@ fetch("https://run.mocky.io/v3/55e3f9c9-df86-46c9-941a-ea43a7595e19")
     </div>`)
     })
   })
+
+  document.querySelector(".form__container").addEventListener("click", sendMessage);
+
+function sendMessage() {
+  let myMessage = document.querySelector(".form__input").value
+
+  containerEl.insertAdjacentHTML("beforeend", `
+  <div class="chat-message__container radius__4">
+        <p class="chat-message__user">Eu</p>
+        <div class="chat-message__content__container">
+          <p class="chat-message__text">${myMessage}</p>
+          <p class="chat-message__hour">12:52</p>
+        </div>
+      </div>
+  `)
+
+  console.log
+}
