de4f645476f7323b1ae6a6e837c6acd9c2d1df8f
Carlos Lopes <dev.carloslopes@gmail.com>
2022-02-21 20:30:39 -0300

    Enter

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const fetchRooms = async (url) => {
+const fetchData = async (url) => {
   const response = await fetch(url);
   
   const json = await response.json();
@@ -6,15 +6,35 @@ const fetchRooms = async (url) => {
   return json;
 }
 
-fetchRooms("https://run.mocky.io/v3/349d9b08-b21b-4134-a500-883efcc6f8f6").then(data => {
+fetchData("https://run.mocky.io/v3/349d9b08-b21b-4134-a500-883efcc6f8f6").then(data => {
   const rooms = data.salas;
-  const roomList = document.getElementById("room-list");
-  rooms.map(room => {
-    const roomItem = document.createElement("li");
-    roomItem.innerHTML = `
-      <h3>${room.name}</h3>
-    `;
+  console.log(rooms)
+  const roomTitle = document.querySelector(".nav__title");
+  const participants = document.querySelector(".chat-participants__title");
 
-    roomList.appendChild(roomItem);
+  rooms.map(room => {
+    roomTitle.innerText = `${room.name}`;
+    participants.innerText = `Participantes (${room.users})`;
+  });
+})
+
+fetchData("https://run.mocky.io/v3/55e3f9c9-df86-46c9-941a-ea43a7595e19").then(data => {
+  const messages = data.messages;
+  console.log(messages)
+  const chatMessage = document.getElementById("chat-message");
+
+  messages.map(message => {
+    chatMessage.innerHTML += `
+    <li class="chat-message__container">
+      <p class="chat-message__user">${message.userName}</p>
+      <div class="chat-message__content__container">
+        <p class="chat-message__text">
+          ${message.message}
+        </p>
+        <p class="chat-message__hour">
+          ${message.date}
+        </p>
+      </div>
+    </li>`;
   });
 })
